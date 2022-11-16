c999c885748d9b933f67af6c5e335fbc57bdf52e
s2223108 <b.j.roskams-hieter@sms.ed.ac.uk>
2022-11-16 15:39:00 +0000

    add script for querying JASPAR database

---
+++ py/pull_jaspar_motifs.py
@@ -0,0 +1,31 @@
+from argparse import ArgumentParser
+from coreapi import Client
+
+def get_input():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "tf", type = str, help="TF for which you want to query motifs from JASPAR"
+    )
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    client = Client()
+    document = client.get('https://jaspar.genereg.net/api/v1/docs/')
+
+    tf = get_input().tf
+    data = client.action(document, ['matrix','list'], params = {
+        'name': tf,
+        'tax_id': '9606',
+        'release': 'JASPAR2022'
+    })
+
+    if data['count'] != 0:
+        for motif in data['results']:
+            id = motif['matrix_id']
+            query = str(f"https://jaspar.genereg.net/api/v1/matrix/{id}/?format=jaspar")
+            pfm = client.get(query)
+            with open(f"{id}.jaspar", 'w') as outfile:
+                outfile.writelines(pfm)
+    else:
+        with open("no_JASPAR_motifs.jaspar", 'w') as outfile:
+                outfile.writelines(f"No motifs in JASPAR for {tf}")
