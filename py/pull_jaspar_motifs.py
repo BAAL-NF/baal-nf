a037ac8f4949fe03a75eddd0e4a8df39c95ecf50
s2223108 <b.j.roskams-hieter@sms.ed.ac.uk>
2023-05-17 16:49:34 +0100

    add option for including cofactors from JASPAR

--- py/pull_jaspar_motifs.py
+++ py/pull_jaspar_motifs.py
@@ -7,6 +7,9 @@ def get_input():
     parser.add_argument(
         "tf", type = str, help="TF for which you want to query motifs from JASPAR"
     )
+    parser.add_argument(
+        "--cofactors", type = bool, nargs='?', const=False, help="True/False for whether you want to also pull motifs for associated proteins"
+    )
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -23,11 +26,18 @@ if __name__ == "__main__":
     if data['count'] != 0:
         os.makedirs(tf)
         for motif in data['results']:
-            if tf in motif['name']:
-                id = motif['matrix_id']
+            if include_cofactors:
+                id = motif['matrix_id'] 
                 query = str(f"https://jaspar.genereg.net/api/v1/matrix/{id}/?format=jaspar")
                 pfm = client.get(query)
                 with open(f"{tf}/{id}.jaspar", 'w') as outfile:
                     outfile.writelines(pfm)
+            else:
+                if tf in motif['name']:
+                    id = motif['matrix_id']
+                    query = str(f"https://jaspar.genereg.net/api/v1/matrix/{id}/?format=jaspar")
+                    pfm = client.get(query)
+                    with open(f"{tf}/{id}.jaspar", 'w') as outfile:
+                        outfile.writelines(pfm)
     else:
         print(f"No motifs in JASPAR for {tf}")
